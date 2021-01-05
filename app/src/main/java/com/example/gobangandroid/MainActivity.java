60314d1021687fa621665b947dac1964772d8b40
2287512349@qq.com <2287512349@qq.com>
2021-01-05 19:12:20 +0800

    commit 2021.1.5 实现点击落子和胜负判定功能 功能比较简单，不能悔棋，背景板也没加载出来

--- app/src/main/java/com/example/gobangandroid/MainActivity.java
+++ app/src/main/java/com/example/gobangandroid/MainActivity.java
@@ -1,58 +1,13 @@
 package com.example.gobangandroid;
 
-import android.graphics.PixelFormat;
-import android.net.Uri;
+import android.app.Activity;
 import android.os.Bundle;
-import android.os.Environment;
-import android.view.View;
-import android.widget.Button;
-import android.widget.MediaController;
-import android.widget.Toast;
-import android.widget.VideoView;
-import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.File;
-
-
-public class MainActivity extends AppCompatActivity {
-
-    private VideoView vvMain;
-    private Button button;
-
-    @Override
-
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends Activity {//继承自Activity
+    WuziqiPanel myView;//myView的引用
+    public void onCreate(Bundle savedInstanceState) {//重写的onCreate方法
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Toast.makeText(MainActivity.this,"因为王家龙不帮我debug所以不能运行",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
-//        vvMain = (VideoView) findViewById(R.id.vv_main);
-//        //播放本地视频
-//        String videoPath= Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES)+"/storage/Pictures/C语言笔记/wjl.mp4";
-//        //播放网络视频
-//        //String videoPath = "http://vfx.mtime.cn/Video/2019/07/12/mp4/190712140656051701.mp4";
-//        //实例化
-//        this.vvMain = vvMain;
-//        //设置播放地址
-//        vvMain.setVideoPath(videoPath);
-//        //开始播放
-//        vvMain.start();
-
-
-
-
-
-
-
-
-
+        myView =  new WuziqiPanel(this,null);//初始化自定义View
+        this.setContentView(myView);//设置当前的用户界面
     }
-
 }
